46c7dd87675d83295d231fd4d9ff84df9bf20d90
zhangbing <zhangbing@163.com>
2020-10-13 22:08:50 +0800

    change

---
+++ PhotoDemo/ContentView.swift
@@ -0,0 +1,42 @@
+//
+//  ContentView.swift
+//  PhotoDemo
+//
+//  Created by WJQ on 2020/10/13.
+//
+
+import SwiftUI
+import SDWebImageSwiftUI
+
+struct ContentView: View {
+    @ObservedObject var randomImages = UnsplashData()
+
+    var body: some View {
+        ScrollView(content: {
+            LazyVStack(content: {
+                ForEach(randomImages.photoArray, id: \.id) { photo in
+                    WebImage(url: URL(string: photo.urls["thumb"]!))
+                        .resizable()
+//                        .placeholder(Image(systemName: "photo"))
+                        .placeholder {
+                            Rectangle().foregroundColor(.gray)
+                        }
+                        .indicator(.activity) // Activity Indicator
+                        .aspectRatio(contentMode: .fill)
+                        .transition(.fade(duration: 0.5)) // Fade Transition with duration
+                        .frame(width: UIScreen.main.bounds.width-50, height: 200, alignment: .center)
+                        .cornerRadius(15)
+
+                        Text("image name\(photo.id)")
+                                
+                }
+            })
+        }).navigationTitle("random images")
+        }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ PhotoDemo/PhotoDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  PhotoDemoApp.swift
+//  PhotoDemo
+//
+//  Created by WJQ on 2020/10/13.
+//
+
+import SwiftUI
+
+@main
+struct PhotoDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
